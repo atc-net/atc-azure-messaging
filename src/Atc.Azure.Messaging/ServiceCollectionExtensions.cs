745a44981de7fb79b2c79e3a91f5adfc6db30218
Christian Helle <christian.helle@outlook.com>
2022-06-15 12:58:28 +0200

    Implement IServiceCollection extension method for configuring options and registering dependencies to the IoC container

---
+++ src/Atc.Azure.Messaging/ServiceCollectionExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Atc.Azure.Messaging.EventHub;
+using Atc.Azure.Messaging.ServiceBus;
+using Atc.Azure.Options.EventHub;
+using Atc.Azure.Options.ServiceBus;
+using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+[ExcludeFromCodeCoverage]
+public static class ServiceCollectionExtensions
+{
+    public static void ConfigureMessagingServices(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddOptions<EventHubOptions>(configuration);
+        services.AddOptions<ServiceBusOptions>(configuration);
+
+        services.AddSingleton<IEventHubPublisherFactory, EventHubPublisherFactory>();
+        services.AddSingleton<IServiceBusClientFactory, ServiceBusClientFactory>();
+        services.AddSingleton<IServiceBusSenderProvider, ServiceBusSenderProvider>();
+    }
+
+    private static T AddOptions<T>(
+        this IServiceCollection services,
+        IConfiguration configuration)
+        where T : class, new()
+    {
+        var options = Activator.CreateInstance<T>();
+
+        configuration.GetSection(options.GetType().Name).Bind(options);
+        services.AddSingleton(options);
+
+        return options;
+    }
+}
