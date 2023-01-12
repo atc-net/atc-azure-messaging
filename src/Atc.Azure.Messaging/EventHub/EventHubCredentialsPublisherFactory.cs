ad4d2883aad5eb924c47f3668bda0dc06ea687db
Anders Tøgersen (Delegate) <ato@delegate.dk]>
2023-01-12 16:38:17 +0100

    Implemented Factories to use TokenCredential Authorization

---
+++ src/Atc.Azure.Messaging/EventHub/EventHubCredentialsPublisherFactory.cs
@@ -0,0 +1,36 @@
+using Atc.Azure.Options.Authorization;
+using Atc.Azure.Options.Environment;
+using Atc.Azure.Options.EventHub;
+using Atc.Azure.Options.Providers;
+using Azure.Identity;
+using Azure.Messaging.EventHubs.Producer;
+
+namespace Atc.Azure.Messaging.EventHub;
+
+#pragma warning disable CA2000 // Dispose objects before losing scope
+
+public class EventHubCredentialsPublisherFactory : IEventHubPublisherFactory
+{
+    private readonly string fullyQualifiedNamespace;
+    private readonly DefaultAzureCredentialOptions credentialOptions;
+
+    public EventHubCredentialsPublisherFactory(
+        EventHubOptions options,
+        EnvironmentOptions environmentOptions,
+        ClientAuthorizationOptions clientCredentialOptions,
+        IAzureCredentialOptionsProvider credentialOptionsProvider)
+    {
+        this.fullyQualifiedNamespace = options.FullyQualifiedNamespace;
+        this.credentialOptions = credentialOptionsProvider
+            .GetAzureCredentialOptions(
+                environmentOptions,
+                clientCredentialOptions);
+    }
+
+    public IEventHubPublisher Create(string eventHubName)
+        => new EventHubPublisher(
+                new EventHubProducerClient(
+                    fullyQualifiedNamespace,
+                    eventHubName,
+                    new DefaultAzureCredential(credentialOptions)));
+}
---
+++ src/Atc.Azure.Messaging/ServiceBus/ServiceBusCredentialsClientFactory.cs
@@ -0,0 +1,31 @@
+using Atc.Azure.Options.Authorization;
+using Atc.Azure.Options.Environment;
+using Atc.Azure.Options.Providers;
+using Atc.Azure.Options.ServiceBus;
+using Azure.Identity;
+using Azure.Messaging.ServiceBus;
+
+namespace Atc.Azure.Messaging.ServiceBus;
+
+public class ServiceBusCredentialsClientFactory : IServiceBusClientFactory
+{
+    private readonly string fullyQualifiedNamespace;
+    private readonly DefaultAzureCredentialOptions credentialOptions;
+
+    public ServiceBusCredentialsClientFactory(
+        ServiceBusOptions options,
+        EnvironmentOptions environmentOptions,
+        ClientAuthorizationOptions clientCredentialOptions,
+        IAzureCredentialOptionsProvider credentialOptionsProvider)
+    {
+        this.fullyQualifiedNamespace = options.FullyQualifiedNamespace;
+        this.credentialOptions = credentialOptionsProvider
+            .GetAzureCredentialOptions(
+                environmentOptions,
+                clientCredentialOptions);
+    }
+
+    public ServiceBusClient Create() => new ServiceBusClient(
+        fullyQualifiedNamespace,
+        new DefaultAzureCredential(credentialOptions));
+}
